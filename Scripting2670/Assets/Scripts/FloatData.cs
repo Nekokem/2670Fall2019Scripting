acc45b5bef332a4b1ec1be8d553c8c2a85e273ac
Katie <kamytherabbit@gmail.com>
2019-09-06 13:51:48 -0600

    Started a new scripting project

---
+++ Scripting2670/Assets/Scripts/FloatData.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+   public float value = 1f;
+   public float maxValue = 1f;
+
+   public void UpdateVale(float amount)
+   {
+
+      value += amount;
+
+   }
+
+   public void UpdateValueLimitZero(float amount)
+   {
+
+      if (value < 0)
+      {
+         value = 0;
+      }
+
+      else
+      {
+         UpdateVale(amount);
+      }
+   }
+
+   public void UpdateValueLimitZeroAndMaxValue(float amount)
+   {
+
+      if (value < maxValue)
+      {
+         UpdateVale(amount);
+      }
+      else
+      {
+         value = maxValue;
+      }
+      UpdateValueLimitZero(amount);
+
+      }
+
+}
---
+++ Scripting2670/Assets/Scripts/ImageController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+[RequireComponent(typeof(Image))]
+public class ImageController : MonoBehaviour
+{
+    private Image imageComponent;
+    public UnityEvent UpdateImageEvent;
+    private void Start()
+    {
+        imageComponent = GetComponent<Image>();
+    }
+
+    public void UpdateImageComponent(float amount)
+    {
+        imageComponent.fillAmount += amount;
+    }
+
+    public void UpdateImageComponent(FloatData dataObj)
+    {
+        imageComponent.fillamount = dataObj.value;
+    }
+    
+    private void Update()
+    {
+        UpdateImageEvent.Invoke();
+    }
+}
