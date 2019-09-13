8df03e5dcb29315c6baa6dae1dc5eda235586c62
Katie <kamytherabbit@gmail.com>
2019-09-13 14:53:14 -0600

    works more then before
    got the image component to work and somewhat of bullet and powerup but actions  and IfStatements not so much

--- Scripting2670/Assets/Scripts/GameActionHandler.cs
+++ Scripting2670/Assets/Scripts/GameActionHandler.cs
@@ -8,10 +8,13 @@ public class GameActionHandler : MonoBehaviour
 
     public UnityEvent handlerEvent;
     public GameAction gameActionObj;
+
+    public IfStatments ifStatmentsObj;
     // Start is called before the first frame update
     void Start()
     {
         gameActionObj.action += Action;
+        ifStatmentsObj.Test += Action;
     }
 
     private void Action()
---
+++ Scripting2670/Assets/Scripts/IfStatments.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IfStatments : MonoBehaviour
+{
+    private float brainFunctionLevel = 75f;
+    private float overLoadLevel = 100f;
+    private float notInUseLevel = 25f;
+
+    public UnityAction Test;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+            LevelTest();
+
+        brainFunctionLevel += Time.deltaTime * 15f;
+
+    }
+
+    void LevelTest()
+    {
+
+        if (brainFunctionLevel > overLoadLevel)
+        {
+
+            print("Can't function.");
+        }
+        
+        else if (brainFunctionLevel < notInUseLevel)
+        {
+            print("Brain is asleep.");
+        }
+
+        else
+        {
+            print("Functioning.");
+        }
+    }
+}
