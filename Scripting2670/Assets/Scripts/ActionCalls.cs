db34dc1e4c93da7e471ae19c842c3def515aef0e
Katie <kamytherabbit@gmail.com>
2019-09-18 14:23:14 -0600

    action practice

---
+++ Scripting2670/Assets/Scripts/ActionCalls.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionCalls : MonoBehaviour
+{
+    public float speed = 1f, rotateSpeed = 10f, scaleSpeed = 0.2f;
+    private Vector3 location, rotationLocations, scales;
+    private UnityAction transformAction;
+    private void Start()
+    {
+        transformAction = OnMove;
+    }
+    private void OnMouseDown()
+   {
+
+       if (transformAction == OnMove)
+       {
+           transformAction = OnRotate;
+       }
+       else if (transformAction == OnRotate)
+       {
+           transformAction = OnScale;
+       }
+       else if (transformAction == OnScale)
+       {
+           transformAction = OnMove;
+       }
+      
+   }
+    void Update()
+   {
+       transformAction();
+   }
+    public void OnMove()
+    {
+        location.x = speed * Time.deltaTime;
+        transform.Translate(location);
+    }
+    public void OnRotate()
+    {
+        rotationLocations.y = rotateSpeed * Time.deltaTime;
+        transform.Rotate(rotationLocations);
+    }
+    public void OnScale()
+    {
+       scales.Set(scaleSpeed, scaleSpeed, scaleSpeed);
+       transform.localScale += scales* Time.deltaTime;
+    }
+}
