c9e353d4c5b19ac84e1a1e7841cfd4927c220838
Katie <kamytherabbit@gmail.com>
2019-09-23 14:46:05 -0600

    Added character mover script

---
+++ Scripting2670/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    public float moveSpeed = 10f, jumpSpeed = 30f, gravity = 3f;
+    private CharacterController Controller;
+    private Vector3 position;
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+        position.z = moveSpeed * Input.GetAxis("Vertical");
+        position.y -= gravity;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            position.y = jumpSpeed;
+        }
+        
+        if (Controller.isGrounded)
+        {
+            position.y = 0;
+        }
+
+        Controller.Move(position * Time.deltaTime);
+    }
+}
