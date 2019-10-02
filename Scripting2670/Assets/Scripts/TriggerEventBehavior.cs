a58cf7e899da5884fd9fdd2f3d079c43da93adb6
Katie <kamytherabbit@gmail.com>
2019-10-02 14:35:07 -0600

    coroutines script

--- Scripting2670/Assets/Scripts/TriggerEventBehavior.cs
+++ Scripting2670/Assets/Scripts/TriggerEventBehavior.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerEventBehavior : MonoBehaviour
 {
-    public UnityEvent TriggerEnterEvent, TriggerStayEvent;
+    public UnityEvent TriggerEnterEvent, TriggerExitEvent;
 
     private void OnTriggerEnter(Collider other)
     {
         TriggerEnterEvent.Invoke();
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerEnterEvent.Invoke();
+    }
 }
