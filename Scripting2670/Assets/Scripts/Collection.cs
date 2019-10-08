1315f62b1d3205456b71dbc6f51316b4d6726f37
Katie <kamytherabbit@gmail.com>
2019-10-07 18:35:43 -0600

    added collections or lists

---
+++ Scripting2670/Assets/Scripts/Collection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Collection : ScriptableObject
+{
+    public List<Object> objectList;
+    
+    
+    public List<FloatData>FloatDataList;
+
+    public List<GameObject> GAmeObjectList;
+
+    public void FindObjectType(Object obj)
+    {
+        foreach (var currentOBJ in objectList)
+        {
+            if (currentOBJ == obj)
+            {
+                //DO WORK;
+            }
+        }
+
+    }
+
+    public void AddToList(Object obj)
+    {
+        objectList.Add(obj);
+    }
+
+    public void RemoveFromList(Object obj)
+    {
+        foreach (var currentOBJ in objectList)
+        {
+            if (currentOBJ == obj)
+            {
+                objectList.Remove(obj);
+            }
+        }
+    }
+}
