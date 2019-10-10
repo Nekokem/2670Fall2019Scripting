7cb722f0989bee38343d39d6f4e16bacccc8be5b
Katie <kamytherabbit@gmail.com>
2019-10-09 19:40:42 -0600

    worked on actions again

--- Scripting2670/Assets/Scripts/GameActionHandler.cs
+++ Scripting2670/Assets/Scripts/GameActionHandler.cs
@@ -6,15 +6,13 @@ public class GameActionHandler : MonoBehaviour
 {
 
 
-    public UnityEvent handlerEvent;
     public GameAction gameActionObj;
+    public UnityEvent handlerEvent;
 
-    public IfStatments ifStatmentsObj;
     // Start is called before the first frame update
     void Start()
     {
         gameActionObj.action += Action;
-        ifStatmentsObj.Test += Action;
     }
 
     private void Action()
